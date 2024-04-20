2963abea971b3d6c1adcdd7f0cffef9cc8afbf83
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-04-21 00:43:41 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  site: "https://muhammedkaplan.com",
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,26 @@
+import { getCollection } from "astro:content";
+
+export async function GET() {
+  const siteUrl = import.meta.env.SITE;
+  const posts = await getCollection("blog");
+  const postsMapped = posts.map((post) => {
+    const lastMod = (post.data.pubDate ?? post.data.pubDate).toISOString();
+    return `<url><loc>${siteUrl}${post.slug}/</loc><lastmod>${lastMod}</lastmod></url>`;
+  });
+
+  const result = `
+        <?xml version="1.0" encoding="UTF-8"?>
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                <url><loc>${siteUrl}/</loc></url>
+                <url><loc>${siteUrl}/blog/</loc></url>
+                <url><loc>${siteUrl}/projects/</loc></url>
+                ${postsMapped.join("\n")}
+            </urlset>
+    `;
+
+  return new Response(result.trim(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
