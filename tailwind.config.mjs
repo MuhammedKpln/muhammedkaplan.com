8c64b6110238512936b940fbc875dc3611a4ae3f
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-04-21 01:06:48 +0200

    fix: badge colors not applied

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	]
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+  safelist: [
+    "bg-red-500",
+    "bg-yellow-500",
+    "bg-green-500",
+    "bg-blue-500",
+    "bg-indigo-500",
+    "bg-purple-500",
+    "bg-pink-500",
+  ],
+};
