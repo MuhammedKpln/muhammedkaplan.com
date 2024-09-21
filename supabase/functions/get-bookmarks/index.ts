5efe9eba1688a2b5484cbdc6bf970ac1bb2ece57
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-09-21 00:56:08 +0000

    fix(functions): add limit query option

--- supabase/functions/get-bookmarks/index.ts
+++ supabase/functions/get-bookmarks/index.ts
@@ -1,9 +1,21 @@
 import { BOOKMARKS_COLLECTION, DATABASE_ID, db } from "@appwrite/client";
+import { Query } from "@appwrite/sdk";
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { successResponse } from "../_shared/http.ts";
 
 Deno.serve(async (req) => {
-  const bookmarks = await db.listDocuments(DATABASE_ID, BOOKMARKS_COLLECTION);
+  const { limit } = await req.json();
+  const queries: string[] = [];
+
+  if (limit) {
+    queries.push(Query.limit(parseInt(limit)));
+  }
+
+  const bookmarks = await db.listDocuments(
+    DATABASE_ID,
+    BOOKMARKS_COLLECTION,
+    queries
+  );
 
   return successResponse(bookmarks);
 });
