526c2a1c569e98591f326a344b2b6d92bd67424b
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2024-09-21 00:59:14 +0000

    fix(functions): use env for appwrite api key

--- supabase/functions/_shared/appwrite.ts
+++ supabase/functions/_shared/appwrite.ts
@@ -8,7 +8,5 @@ export const db = new Databases(client);
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("66edd9e5001de168906c")
-  .setKey(
-    "standard_7b8299de088c45a65da0e0a582ae5310d151ed6417a5c37cbd0146ef8c7383a66dc359ceb9986f64bdc3d4ddc0973f39fe00c42c527deb6163a761ed0d4a6f7800f58b46d90523640cc5d35edb15e8e567dbfef2d07975d5e654bae296e38cd13f02e2683115193caa2243cf1248fff896eb8e9ce97fd43f9d2b42b8cda7b4d8"
-  );
+  .setProject(Deno.env.get("APPWRITE_PROJECT_ID")!)
+  .setKey(Deno.env.get("APPWRITE_API_KEY")!);
